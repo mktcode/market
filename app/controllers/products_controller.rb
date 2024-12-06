ab6c91b0fa1ae7ad547306959ac1dae8a17b96bb
mktcode <kontakt@markus-kottlaender.de>
2024-12-06 17:19:50 +0100

    following guide

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,16 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "products#index"
 
-  get "products" => "products#index", as: :products
+  resources :products
 end
