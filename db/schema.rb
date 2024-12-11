509b90031c9551c2bc47e6bbc24163c9f1a9f26e
mktcode <kontakt@markus-kottlaender.de>
2024-12-11 16:26:16 +0100

    chore: change user_id column type from integer to bigint in sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_10_145715) do
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "ip_address"
     t.string "user_agent"
     t.datetime "created_at", null: false
