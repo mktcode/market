2c4d4208bb1f01344afb723d0c1dd493cca5b94c
mktcode <kontakt@markus-kottlaender.de>
2024-12-11 11:54:12 +0100

    test: fix controller test
    See previous commit. Not sure this is the "correct" way to do it.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,19 +3,23 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @user = users(:one)
   end
 
   test "should get index" do
+    post session_url, params: { email_address: @user.email_address, password: "password" }
     get products_url
     assert_response :success
   end
 
   test "should get new" do
+    post session_url, params: { email_address: @user.email_address, password: "password" }
     get new_product_url
     assert_response :success
   end
 
   test "should create product" do
+    post session_url, params: { email_address: @user.email_address, password: "password" }
     assert_difference("Product.count") do
       post products_url, params: { product: { body: @product.body, title: @product.title } }
     end
@@ -24,21 +28,25 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show product" do
+    post session_url, params: { email_address: @user.email_address, password: "password" }
     get product_url(@product)
     assert_response :success
   end
 
   test "should get edit" do
+    post session_url, params: { email_address: @user.email_address, password: "password" }
     get edit_product_url(@product)
     assert_response :success
   end
 
   test "should update product" do
+    post session_url, params: { email_address: @user.email_address, password: "password" }
     patch product_url(@product), params: { product: { body: @product.body, title: @product.title } }
     assert_redirected_to product_url(@product)
   end
 
   test "should destroy product" do
+    post session_url, params: { email_address: @user.email_address, password: "password" }
     assert_difference("Product.count", -1) do
       delete product_url(@product)
     end
