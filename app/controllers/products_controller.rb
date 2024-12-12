5354bc26c48bfb78c5ce81a808e2cac9ecd752f5
mktcode <kontakt@markus-kottlaender.de>
2024-12-12 18:47:09 +0100

    layout changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.expect(product: [ :title, :body ])
+      params.expect(product: [ :title, :body, images: [] ])
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   has_rich_text :body
+  has_many_attached :images
 end
