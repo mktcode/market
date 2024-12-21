9092c779d92745b2ca8b60b3a9e87019f21ee70d
mktcode <kontakt@markus-kottlaender.de>
2024-12-21 15:11:24 +0100

    feat: allow unauthenticated access to explore page in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  allow_unauthenticated_access only: :index
+  allow_unauthenticated_access only: [ :index, :explore ]
 
   def index
     @products = Product.where(published: true).order(created_at: :desc).limit(4)
