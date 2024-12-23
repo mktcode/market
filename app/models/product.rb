2502506d19e89caee89492efbfe384c6e1a3e90a
mktcode <kontakt@markus-kottlaender.de>
2024-12-23 09:37:39 +0100

    feat: add image variants for products and users to optimize display

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,9 @@ class Product < ApplicationRecord
   has_many :materials, dependent: :destroy
   accepts_nested_attributes_for :materials, allow_destroy: true, reject_if: lambda { |attributes| attributes["name"].blank? }
   has_rich_text :body
-  has_many_attached :images
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_fill: [ 350, 350 ]
+  end
 
   def ordered_images
     images.sort_by(&:id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :products, dependent: :destroy
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_fill: [ 100, 100 ]
+  end
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   def to_param
