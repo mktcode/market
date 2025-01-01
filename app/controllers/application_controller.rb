7c38823750be516816ca7476344ca6a7a5e4eabf
mktcode <kontakt@markus-kottlaender.de>
2025-01-01 15:31:08 +0100

    feat: add unread messages count to navbar and update current attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   include Authentication
   before_action :load_session_if_available
+  before_action :unread_messages_count
   allow_browser versions: :modern
 
   private
 
   def load_session_if_available
     authenticated?
+  end
+
+  def unread_messages_count
+    return 0 unless Current.user
+    Current.unread_messages_count = 1
   end
 end
--- app/models/current.rb
+++ app/models/current.rb
@@ -1,4 +1,5 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :session
   delegate :user, to: :session, allow_nil: true
+  attribute :unread_messages_count, default: 0
 end
